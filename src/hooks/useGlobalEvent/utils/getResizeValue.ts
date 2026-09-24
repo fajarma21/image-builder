b280423d06cc1fef019359bf23fcb9206630f815
Fajar Maulana <47219560+fajarma21@users.noreply.github.com>
2026-09-24 16:41:29 +0700

    feat(resize): fixed corner resize (#1)

--- src/hooks/useGlobalEvent/utils/getResizeValue.ts
+++ src/hooks/useGlobalEvent/utils/getResizeValue.ts
@@ -1,35 +1,82 @@
+import viewportToCanvas from '@/utils/viewportToCanvas';
 import type { ResizingInteraction } from '@/types/interaction';
-import getDeltaAxis from '@/utils/getDeltaAxis';
+import type { Camera } from '@/types';
 
 const getResizeValue = (
   e: MouseEvent,
+  canvas: SVGSVGElement,
+  camera: Camera,
   interaction: ResizingInteraction,
-  zoom: number,
 ) => {
-  const currentMouse = { x: e.clientX, y: e.clientY };
-  const { dx, dy } = getDeltaAxis(interaction.startMouse, currentMouse, zoom);
+  const startShape = interaction.startShapes[0];
+  const rad = (startShape.rotation * Math.PI) / 180;
 
-  const shape = interaction.startShapes[0];
-  const rad = (shape.rotation * Math.PI) / 180;
-  const cos = Math.cos(rad);
-  const sin = Math.sin(rad);
+  const localX = {
+    x: Math.cos(rad),
+    y: Math.sin(rad),
+  };
 
-  const localDx = (dx * cos + dy * sin) * 2;
-  const localDy = (-dx * sin + dy * cos) * 2;
-  const maxDiff = Math.max(localDx, localDy);
+  const localY = {
+    x: -Math.sin(rad),
+    y: Math.cos(rad),
+  };
 
-  const width = Math.max(1, shape.width + (e.shiftKey ? localDx : maxDiff));
-  const height = Math.max(1, shape.height + (e.shiftKey ? localDy : maxDiff));
+  const { canvasX, canvasY } = viewportToCanvas(
+    e.clientX,
+    e.clientY,
+    camera,
+    canvas,
+  );
 
-  const x = interaction.center.x - width / 2;
-  const y = interaction.center.y - height / 2;
+  const mouseVector = {
+    x: canvasX - interaction.anchor.x,
+    y: canvasY - interaction.anchor.y,
+  };
+
+  const width = mouseVector.x * localX.x + mouseVector.y * localX.y;
+  const height = mouseVector.x * localY.x + mouseVector.y * localY.y;
+  let newWidth = Math.max(width, 1);
+  let newHeight = Math.max(height, 1);
+
+  if (!e.shiftKey) {
+    const cornerVector = {
+      x: localX.x * startShape.width + localY.x * startShape.height,
+      y: localX.y * startShape.width + localY.y * startShape.height,
+    };
+    const dot = mouseVector.x * cornerVector.x + mouseVector.y * cornerVector.y;
+    const lengthSquared =
+      cornerVector.x * cornerVector.x + cornerVector.y * cornerVector.y;
+
+    const scale = Math.max(0, dot / lengthSquared);
+    const minScale = Math.max(1 / startShape.width, 1 / startShape.height);
+    const finalScale = Math.max(scale, minScale);
+
+    newWidth = startShape.width * finalScale;
+    newHeight = startShape.height * finalScale;
+  }
+
+  const center = {
+    x:
+      interaction.anchor.x +
+      localX.x * (newWidth / 2) +
+      localY.x * (newHeight / 2),
+
+    y:
+      interaction.anchor.y +
+      localX.y * (newWidth / 2) +
+      localY.y * (newHeight / 2),
+  };
+
+  const newX = center.x - newWidth / 2;
+  const newY = center.y - newHeight / 2;
 
   return {
-    id: shape.id,
-    x: Math.round(x),
-    y: Math.round(y),
-    width,
-    height,
+    id: startShape.id,
+    x: newX,
+    y: newY,
+    width: newWidth,
+    height: newHeight,
+    rotation: startShape.rotation,
   };
 };
 
